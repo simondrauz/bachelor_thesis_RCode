588dc63099e18ddcff326a247580866bfca31589
Simon Drauz <s.drauz@gmail.com>
2023-11-04 14:37:00 +0100

    (feat) add stan drivers which enable multi-sessions

---
+++ stan_drivers/stan_startup_file.R
@@ -0,0 +1,24 @@
+#This file is used to set overall specifications for the run
+
+# LOAD NEEDED PACKAGES
+library("rstan")
+library('arrow')
+library('dplyr')
+library('parallel')
+
+# Set Running variables
+distribution_assumption = "nb"
+stan_models_dir_path = "C:/Users/Uwe Drauz/RProjects/bachelor_thesis/stan_models/"
+# Set to "_enableParallel" if parallel computing should be activated
+enable_parallel = ""
+model = "model1"
+
+feature_set = "feature_set2"
+data_dir_path = "C:/Users/Uwe Drauz/Documents/bachelor_thesis_local/personal_competition_data/data/"
+
+stan_fit_dir = "C:/Users/Uwe Drauz/RProjects/bachelor_thesis/stan_fits/"
+
+eval_months <- c(457)
+lag_indicators <- c(3)
+no_chains = 1
+cores_for_par = 0
